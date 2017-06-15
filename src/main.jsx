f3cb24149f352c9fb9773d1956b141c21cc49314
faithngetich <faith.ngetich@andela.com>
2017-06-15 12:13:57 +0300

    [Add skeleton functionality]

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/ListItem.jsx');
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
